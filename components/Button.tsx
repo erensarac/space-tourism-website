6ea0be9c87cdc6c51e67cf52e3e97bcd10e43b9b
erensarac <esarac@outlook.com.tr>
2024-08-03 23:38:46 +0300

    refactor(Button): update Button component prop types

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,5 @@
-export default function Button({ children }: Readonly<{
-   children: React.ReactNode;
-}>) {
+export default function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
    return (
-      <button className="transition-[box-shadow, colors] ring-offset-0	 duration-500 hover:shadow-button hover:ring-button ring-white/10 hover:text-blue-900/50 text-preset-4 uppercase text-blue-900 size-[144px] md:size-[272px] rounded-full bg-white flex justify-center items-center">{children}</button>
+      <button {...props} className="transition-[box-shadow, colors] ring-offset-0 duration-500 hover:shadow-button hover:ring-button ring-white/10 hover:text-blue-900/50 text-preset-4 uppercase text-blue-900 size-[144px] md:size-[272px] rounded-full bg-white flex justify-center items-center">{children}</button>
    )
 }
