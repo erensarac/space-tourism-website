265e4f5488df54973ecca177387c6e0f2df0ddab
erensarac <esarac@outlook.com.tr>
2024-08-03 19:37:29 +0300

    feat: add Hamburger and Close icon

---
+++ components/icons/Hamburger.tsx
@@ -0,0 +1,14 @@
+export default function Hamburger({ width = 24, height = 21, ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      {...props}
+    >
+      <g fill="#D0D6F9" fillRule="evenodd">
+        <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
+      </g>
+    </svg>
+  );
+}
