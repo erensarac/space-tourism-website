c8d9ae6fe00e3864ad5878de605f67da169d361f
erensarac <esarac@outlook.com.tr>
2024-08-16 21:48:38 +0300

    refactor: update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,31 @@
-'use client'
+"use client";
 import { useRouter } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import Button from "@/components/Button";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
-    <main
-      className="bg-mobile-home md:bg-tablet-home lg:bg-desktop-home bg-cover bg-no-repeat flex flex-col min-h-screen w-full bg-blue-900"
-    >
+    <main className="flex min-h-screen w-full flex-col bg-blue-900 bg-mobile-home bg-cover bg-no-repeat md:bg-tablet-home lg:bg-desktop-home">
       <Navbar />
-      <section className="flex-1 sm:flex-auto p-300 sm:px-500 gap-[66px] lg:gap-[30px] sm:py-[128px] flex flex-col xl:flex-row items-center xl:items-end justify-center">
-        <div className="text-center xl:text-left  max-w-[560px] xl:max-w-[540px] xl:items-start xl:justify-start w-full flex flex-col gap-300">
-          <span className="leading-[1.1875rem] md:leading-[2.125rem] uppercase text-preset-5 text-blue-300">So, you want to travel to</span>
-          <h1 className="lg:indent-[-10px] uppercase text-preset-1 text-white">Space</h1>
-          <p className="text-preset-9 text-blue-300">
-            Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+      <section className="flex flex-1 flex-col items-center justify-center p-2 sm:flex-auto sm:px-4 sm:py-9 md:gap-10 lg:gap-5 xl:flex-row xl:items-end">
+        <div className="flex w-full max-w-3 flex-col gap-4 text-center xl:max-w-4 xl:items-start xl:justify-start xl:text-left">
+          <span className="text-preset-6 md:text-preset-5-lg uppercase text-blue-300">
+            So, you want to travel to
+          </span>
+          <h1 className="text-preset-1 md:text-preset-1-lg uppercase text-white">
+            Space
+          </h1>
+          <p className="text-preset-9 md:text-preset-9-md lg:text-preset-9-lg text-blue-300">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
           </p>
         </div>
-        <div className="flex-1 items-center sm:items-start sm:flex-auto flex justify-center xl:justify-end xl:max-w-[540px] w-full">
-          <Button onClick={() => router.push('/destination')}>Explore</Button>
+        <div className="flex max-h-1 w-full flex-1 items-center justify-center sm:flex-auto sm:items-start xl:max-w-4 xl:justify-end">
+          <Button onClick={() => router.push("/destination")}>Explore</Button>
         </div>
       </section>
     </main>
