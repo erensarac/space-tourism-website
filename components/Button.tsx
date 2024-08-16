cac7abbdc31a3eb45b881278b7b2aabd1bab1a98
erensarac <esarac@outlook.com.tr>
2024-08-16 21:49:19 +0300

    refactor: update Button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,13 @@
-export default function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-   return (
-      <button {...props} className="transition-[box-shadow, colors] ring-offset-0 duration-500 hover:shadow-button hover:ring-button ring-white/10 hover:text-blue-900/50 text-preset-4 uppercase text-blue-900 size-[144px] md:size-[272px] rounded-full bg-white flex justify-center items-center">{children}</button>
-   )
+export default function Button({
+  children,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className="transition-[box-shadow, colors] text-preset-4 md:text-preset-4-lg flex size-5 items-center justify-center rounded-full bg-white uppercase text-blue-900 ring-white/10 ring-offset-0 duration-500 hover:text-blue-900/50 hover:shadow-button hover:ring-button md:size-7"
+    >
+      {children}
+    </button>
+  );
 }
