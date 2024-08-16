010a90b72ef3457147f214827985f85bebbdcc46
erensarac <esarac@outlook.com.tr>
2024-08-16 21:45:28 +0300

    refactor: update Tabs component

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,44 +1,34 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-
-const list = [
-   {
-      text: "Moon",
-      query: "moon"
-   },
-   {
-      text: "Mars",
-      query: "mars"
-   },
-   {
-      text: "Europa",
-      query: "europa"
-   },
-   {
-      text: "Titan",
-      query: "titan"
-   },
-]
+import { planets } from "@/app/constants";
 
 export default function Tabs() {
-   const searchParams = useSearchParams()
-   const params = new URLSearchParams(searchParams.toString())
-   const planet = params.get("planet");
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams.toString());
+  const planet = params.get("planet");
 
-   return (
-      <div className="flex gap-400">
-         {list.map(item => (
-            <div key={item.query} className="group flex h-400 relative">
-               <Link className="text-preset-8 uppercase text-blue-300" scroll={false} href={{
-                  pathname: '/destination',
-                  query: { planet: item.query },
-               }}>
-                  {item.text}
-               </Link>
-               <hr className={`${planet === item.query || (!planet && item.query === 'moon') ? 'bg-white' : 'group-hover:bg-white/50 bg-white/0'} absolute left-0 bottom-0 w-full h-[3px] border-none`} />
-            </div>
-         ))}
-      </div>
-   )
+  return (
+    <div className="flex gap-6">
+      {planets
+        .sort((a, b) => a.order - b.order)
+        .map((item) => (
+          <div key={item.query} className="group relative flex h-3">
+            <Link
+              className="text-preset-8 md:text-preset-8-lg uppercase text-blue-300"
+              scroll={false}
+              href={{
+                pathname: "/destination",
+                query: { planet: item.query },
+              }}
+            >
+              {item.name}
+            </Link>
+            <hr
+              className={`${planet === item.query || (!planet && item.query === "moon") ? "bg-white" : "bg-white/0 group-hover:bg-white/50"} absolute bottom-0 left-0 h-2 w-full border-none`}
+            />
+          </div>
+        ))}
+    </div>
+  );
 }
