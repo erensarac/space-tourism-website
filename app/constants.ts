d54e5f0c39ef67711d59761ddb51720ab269b56a
erensarac <esarac@outlook.com.tr>
2024-08-16 21:40:35 +0300

    feat: add constansts file for planets, members, technologies

---
+++ app/constants.ts
@@ -0,0 +1,137 @@
+export const planets = [
+  {
+    order: 1,
+    name: "Moon",
+    query: "moon",
+    description:
+      "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
+    averageDistance: "384,400 km",
+    travelTime: "3 days",
+    imageSource: "/assets/destination/image-moon.png",
+    imageAlt: "Moon Image",
+  },
+  {
+    order: 2,
+    name: "Mars",
+    query: "mars",
+    description:
+      "Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
+    averageDistance: "225 mil. km",
+    travelTime: "9 months",
+    imageSource: "/assets/destination/image-mars.png",
+    imageAlt: "Mars Image",
+  },
+  {
+    order: 3,
+    name: "Europa",
+    query: "europa",
+    description:
+      "The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
+    averageDistance: "628 mil. km",
+    travelTime: "3 years",
+    imageSource: "/assets/destination/image-europa.png",
+    imageAlt: "Europa Image",
+  },
+  {
+    order: 4,
+    name: "Titan",
+    query: "titan",
+    description:
+      "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
+    averageDistance: "1.6 bil. km",
+    travelTime: "7 years",
+    imageSource: "/assets/destination/image-titan.png",
+    imageAlt: "Titan Image",
+  },
+];
+
+export const members = [
+  {
+    order: 1,
+    query: "douglas-hurley",
+    name: "Douglas Hurley",
+    rank: "Commander",
+    description:
+      "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+    imageSource: "/assets/crew/image-douglas-hurley.webp",
+    imageAlt: "Douglas Hurley",
+  },
+  {
+    order: 2,
+    query: "mark-shuttleworth",
+    name: "MARK SHUTTLEWORTH",
+    rank: "Mission Specialist",
+    description:
+      "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+    imageSource: "/assets/crew/image-mark-shuttleworth.webp",
+    imageAlt: "Mark Shuttleworth",
+  },
+  {
+    order: 3,
+    query: "victor-glover",
+    name: "Victor Glover",
+    rank: "PILOT",
+    description:
+      "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.",
+    imageSource: "/assets/crew/image-victor-glover.webp",
+    imageAlt: "Victor Glover",
+  },
+  {
+    order: 4,
+    query: "anousheh-ansari",
+    name: "Anousheh Ansari",
+    rank: "Flight Engineer",
+    description:
+      "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.",
+    imageSource: "/assets/crew/image-anousheh-ansari.webp",
+    imageAlt: "Anousheh Ansari",
+  },
+];
+
+export const technologies = [
+  {
+    order: 1,
+    name: "launch-vehicle",
+    title: "Launch Vehice",
+    description:
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+    landspace: {
+      src: "/assets/technology/image-launch-vehicle-landscape.jpg",
+      alt: "Launch Vehicle",
+    },
+    portrait: {
+      src: "/assets/technology/image-launch-vehicle-portrait.jpg",
+      alt: "Launch Vehicle",
+    },
+  },
+  {
+    order: 2,
+    name: "spaceport",
+    title: "Spaceport",
+    description:
+      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
+    landspace: {
+      src: "/assets/technology/image-spaceport-landscape.jpg",
+      alt: "Spaceport",
+    },
+    portrait: {
+      src: "/assets/technology/image-spaceport-portrait.jpg",
+      alt: "Spaceport",
+    },
+  },
+  {
+    order: 3,
+    name: "space-capsule",
+    title: "Space Capsule",
+    description:
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+    landspace: {
+      src: "/assets/technology/image-space-capsule-landscape.jpg",
+      alt: "Space Capsule",
+    },
+    portrait: {
+      src: "/assets/technology/image-space-capsule-portrait.jpg",
+      alt: "Space Capsule",
+    },
+  },
+];
